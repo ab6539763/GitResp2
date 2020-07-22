cea534f7cde81c788727c675ac4d31786c82fb0e
xiao <xiao@qq.com>
2020-07-22 15:50:47 +0800

    update2 Test.java

--- src/com/itcast/cn/Test.java
+++ src/com/itcast/cn/Test.java
@@ -3,5 +3,8 @@ package com.itcast.cn;
 public class Test {
     public static void main(String[] args) {
         System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
+        System.out.println("+++++++++++++++++");
     }
 }
