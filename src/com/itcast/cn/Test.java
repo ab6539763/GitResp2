131818c79f8e5c727b09b0a0280a6213a7995b04
xiao <xiao@qq.com>
2020-07-22 16:02:03 +0800

    update7 Test.java

--- src/com/itcast/cn/Test.java
+++ src/com/itcast/cn/Test.java
@@ -6,10 +6,9 @@ public class Test {
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
-
-
-
-
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("++++++");
+        }
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
         System.out.println("+++++++++++++++++");
